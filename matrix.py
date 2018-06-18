368f2decc445aa115e26db7b26bc88d422263a99
pitmod <piotr.modlinger@gmail.com>
2018-06-18 19:04:38 +0200

    Added numpy delete column and row examples

--- matrix.py
+++ matrix.py
@@ -16,10 +16,12 @@ print(m[0])
 print(m[1][1])
 
 # Add row at the end (0 defines an axis x so row)
+print('# Add row')
 m_r = append(m,[['Avg',12,15,22,22]],0)
 print(m_r)
 print(20*'-')
 
+print('# Add column')
 m_z = append(m,[['A'],['B'],['C'],['D']],1)
 print(m_z)
 print(20*'-')
@@ -30,4 +32,20 @@ m_c = insert(m,[1],[[1],[2],[3],[4]],1)
 print(m_c)
 print(20*'-')
 
+
+# Delete row
+print('# Delete row')
+print('Before:\n',m)
+m = delete(m,[2],0)
+print(20*'-')
+print('After:\n',m)
+print(20*'-')
+
+
+# Delete col
+print('# Delete col')
+print('Before:\n',m)
+print(20*'-')
+m = delete(m,[0],1)
+print('After:\n',m)
 
